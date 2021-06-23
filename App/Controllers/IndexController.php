0dd044e901d3a5c5d726d5b4174ef812340a6b46
Luis Felipe <lipebezerraa@gmail.com>
2021-06-22 21:34:19 -0300

    Retirando comentarios e códigos antigos

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -6,21 +6,17 @@ namespace App\Controllers;
 use MF\Controller\Action;
 use MF\Model\Container;
 
- 
+
 //os models
-use App\Models\Produto; 
+use App\Models\Produto;
 use App\Models\Info;
 
- 
-class IndexController extends Action {
 
-	public function index() {
-		//$this->view->dados = array("sofa","Mesa","cadeira");
-	
+class IndexController extends Action
+{
+
+	public function index()
+	{
 		$this->render('index');
 	}
-
-	
-
-
 }
