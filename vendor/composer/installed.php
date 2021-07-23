a84b330641c96868371a338b15c89ab91cedc27a
Felipe118 <lipebezerraa@gmail.com>
2021-07-22 22:06:35 -0300

    Botando projeto para rodar novamente apos problemas com o composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '23e4cc2a8d03421fe22430f6cc6cedb7d59610e0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '23e4cc2a8d03421fe22430f6cc6cedb7d59610e0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
