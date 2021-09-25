35f54fc9026b079a05cb7966db17782b91f16e7f
Felipe118 <lipebezerraa@gmail.com>
2021-09-24 21:53:48 -0300

    Proximas Tarefas é ajeitar a home trazer a imagem a esquerda e a direita o titulo da materia estilo globo esporte

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -4,17 +4,24 @@
 
    //os recursos do miniframework
    use MF\Controller\Action;
-   use MF\Model\Container;
+   use MF\Model\Container; 
+   use MF\Model\Model\Noticia;
+   
  
    class HomeController extends Action{ 
     public function home(){
 
+
         session_start(); 
 
-        if($_SESSION['autenticado'] == false){
+       if($_SESSION['autenticado'] == false){
           header('Location: /'); 
         }
         $this->renderHome('home');
+
+
+        $noticia = Container::getModel('Noticia');
+
     }
     public function home_adm(){
 
